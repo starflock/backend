1d8d40a7fe6d405b42a5d3ed6674ae63a93647d6
Nick Vladimiroff <nvladimiroff@overdrive.com>
2018-10-20 14:51:07 -0400

    Add a database!

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 from flask import *
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = urlparse.urlparse(os.environ["DATABASE_URL"])
+db = SQLAlchemy(app)
+
+class FireReport(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    lat = db.Column(db.String(80))
+    lon = db.Column(db.String(80))
+    device_id = db.Column(db.String(80))
+    timestamp = db.Column(db.String(80))
+
+db.create_all()
 
 @app.route('/fires', methods=['GET', 'POST'])
 def fires():
@@ -9,7 +22,10 @@ def fires():
         return find_fires()
 
 def report_fire():
-    print(request.json)
+    report = request.json
+
+    db.session.add(FireReport(lat = report['location']['latitude'], lon = report['location']['longitude'], device_id = report['device_id'], timestamp = report['timestamp']))
+    db.commit()
     return ('', 201)
 
 def find_fires():
