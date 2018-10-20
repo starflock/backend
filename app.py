0c2dc8306c8e0362dd329b1cd0977c48cb2a4132
Nick Vladimiroff <nvladimiroff@overdrive.com>
2018-10-20 13:50:13 -0400

    Remove a bunch of stuff!

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import *
+app = Flask(__name__)
+
+@app.route('/fires', methods=['GET', 'POST'])
+def fires():
+    if request.method == 'POST':
+        return report_fire()
+    else:
+        return find_fires()
+
+def report_fire():
+    print reuqest.json
+    return ('', 201)
+
+def find_fires():
+    return ('hello', 200)
