5624e37a9897f6faf8e12a18670830570cfd1a59
ortizub41 <ortizub41@gmail.com>
2018-10-20 23:42:27 -0400

    Removing pdb

--- app.py
+++ app.py
@@ -88,7 +88,6 @@ def sms_reply():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     try:
-        import pdb; pdb.set_trace()
         if request.method == "POST":
             user_credentials = request.json
             email = user_credentials["email"]
